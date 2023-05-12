97f491850e8f43a6abe391035513aa7e38a02a3e
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-05-12 17:46:17 +0900

    Test code for Uncomprehended IE

--- ngap_test.go
+++ ngap_test.go
@@ -2,9 +2,12 @@ package ngap
 
 import (
 	"encoding/hex"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/free5gc/ngap/ngapType"
 )
 
 func TestDecodeBackupAmfName(t *testing.T) {
@@ -14,4 +17,110 @@ func TestDecodeBackupAmfName(t *testing.T) {
 	require.NoError(t, err)
 	_, err = Decoder(bs)
 	require.NoError(t, err)
+}
+
+func TestDecoder(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantPdu *ngapType.NGAPPDU
+		wantErr bool
+	}{
+		{
+			name: "TestUncomprehendedProcedure",
+			args: args{
+				b: []byte{
+					0x00, 0x55, 0x00, 0x17, 0x00, 0x00, 0x02, 0x00, 0x52, 0x40, 0x06, 0x01, 0x80, 0x54, 0x45, 0x53,
+					0x54, 0x00, 0x52, 0x40, 0x06, 0x01, 0x80, 0x54, 0x45, 0x53, 0x54,
+				},
+			},
+			wantPdu: &ngapType.NGAPPDU{
+				Present: ngapType.NGAPPDUPresentInitiatingMessage,
+				InitiatingMessage: &ngapType.InitiatingMessage{
+					ProcedureCode: ngapType.ProcedureCode{
+						Value: 0x55,
+					},
+					Criticality: ngapType.Criticality{
+						Value: ngapType.CriticalityPresentReject,
+					},
+					Value: ngapType.InitiatingMessageValue{
+						Present: 0,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "TestUncomprehendedIE",
+			args: args{
+				b: []byte{
+					0x00, 0x15, 0x00, 0x17, 0x00, 0x00, 0x02, 0x55, 0x55, 0x40, 0x06, 0x01, 0x80, 0x54, 0x45, 0x53,
+					0x54, 0x00, 0x52, 0x40, 0x06, 0x01, 0x80, 0x54, 0x45, 0x53, 0x54,
+				},
+			},
+			wantPdu: &ngapType.NGAPPDU{
+				Present: ngapType.NGAPPDUPresentInitiatingMessage,
+				InitiatingMessage: &ngapType.InitiatingMessage{
+					ProcedureCode: ngapType.ProcedureCode{
+						Value: ngapType.ProcedureCodeNGSetup,
+					},
+					Criticality: ngapType.Criticality{
+						Value: ngapType.CriticalityPresentReject,
+					},
+					Value: ngapType.InitiatingMessageValue{
+						Present: ngapType.InitiatingMessagePresentNGSetupRequest,
+						NGSetupRequest: &ngapType.NGSetupRequest{
+							ProtocolIEs: ngapType.ProtocolIEContainerNGSetupRequestIEs{
+								List: []ngapType.NGSetupRequestIEs{
+									{
+										Id: ngapType.ProtocolIEID{
+											Value: 0x5555,
+										},
+										Criticality: ngapType.Criticality{
+											Value: ngapType.CriticalityPresentIgnore,
+										},
+										Value: ngapType.NGSetupRequestIEsValue{
+											Present: 0,
+										},
+									},
+									{
+										Id: ngapType.ProtocolIEID{
+											Value: ngapType.ProtocolIEIDRANNodeName,
+										},
+										Criticality: ngapType.Criticality{
+											Value: ngapType.CriticalityPresentIgnore,
+										},
+										Value: ngapType.NGSetupRequestIEsValue{
+											Present: ngapType.NGSetupRequestIEsPresentRANNodeName,
+											RANNodeName: &ngapType.RANNodeName{
+												Value: "TEST",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			gotPdu, err := Decoder(tt.args.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Decoder() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotPdu, tt.wantPdu) {
+				t.Errorf("Decoder() = %v, want %v", gotPdu, tt.wantPdu)
+			}
+		})
+	}
 }
