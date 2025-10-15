43525e955a7961cb0c7f962ff571b1ca16b8713b
Alonza0314 <alonza.tu@gmail.com>
2025-10-15 08:59:06 +0000

    feat: additional tnl information used for dc handover

--- ngapType/PathSwitchRequestAcknowledgeTransfer.go
+++ ngapType/PathSwitchRequestAcknowledgeTransfer.go
@@ -3,7 +3,8 @@ package ngapType
 // Need to import "github.com/free5gc/aper" if it uses "aper"
 
 type PathSwitchRequestAcknowledgeTransfer struct {
-	ULNGUUPTNLInformation *UPTransportLayerInformation                                          `aper:"valueLB:0,valueUB:1,optional"`
-	SecurityIndication    *SecurityIndication                                                   `aper:"valueExt,optional"`
-	IEExtensions          *ProtocolExtensionContainerPathSwitchRequestAcknowledgeTransferExtIEs `aper:"optional"`
+	ULNGUUPTNLInformation         *UPTransportLayerInformation                                          `aper:"valueLB:0,valueUB:1,optional"`
+	SecurityIndication            *SecurityIndication                                                   `aper:"valueExt,optional"`
+	AdditionalNGUUPTNLInformation *UPTransportLayerInformationPairList                                  `aper:"optional"`
+	IEExtensions                  *ProtocolExtensionContainerPathSwitchRequestAcknowledgeTransferExtIEs `aper:"optional"`
 }
